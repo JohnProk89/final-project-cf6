0e70c008d57bfe18e61c3a6ecd1c0969f8672050
Giannis Prokopiou <johnprok89@gmail.com>
2025-01-27 04:29:40 +0200

    login fix

--- src/main/java/gr/aueb/cf/finalprojectcf6/repository/UserRepository.java
+++ src/main/java/gr/aueb/cf/finalprojectcf6/repository/UserRepository.java
@@ -3,6 +3,8 @@ package gr.aueb.cf.finalprojectcf6.repository;
 import gr.aueb.cf.finalprojectcf6.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsernameAndPassword(String username, String password);
+    Optional<User> findByUsernameAndPassword(String username, String password);
 }
